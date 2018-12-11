6805c130bb88a4036d60f6ee283b9748c498ac52
Rachel Hofer <rhofer3@gmail.com>
2018-12-10 20:05:13 -0500

    property routes tested and complete. last route to add is search by zipcode

--- models/Property.js
+++ models/Property.js
@@ -5,7 +5,9 @@ const Schema   = mongoose.Schema;
 const propertySchema = new Schema({
   image: String,
   address: String,
-  features: {type: Array, enum: ['lights', 'decorations', 'music', 'carolers', 'holiday beverages']},
+  features: {type: [String], enum: ['lights', 'decorations', 'music', 'carolers', 'holiday beverages']},
+  review: {type: [Schema.Types.ObjectId], ref: 'Review'},
+  creator: {type: Schema.Types.ObjectId, ref: 'User'},
   averageRating: Number
   });
 
