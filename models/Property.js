0efaf05ea652d3449dfc7ac680a0c9d3d1e24549
Rustam_Samigullin <rstsmg1@gmail.com>
2018-12-10 20:52:33 -0500

    changed property model

--- models/Property.js
+++ models/Property.js
@@ -3,9 +3,12 @@ const Schema   = mongoose.Schema;
 
 
 const propertySchema = new Schema({
-  image: String,
-  address: String,
-  features: String,
+  image   : String,
+  address : String,
+  zipCode : Number,
+  features: {type: [String], enum: ['lights', 'decorations', 'music', 'carolers', 'holiday beverages']},
+  review  : {type: [Schema.Types.ObjectId], ref: 'Review'},
+  creator : {type: Schema.Types.ObjectId, ref: 'User'},
   averageRating: Number
   
   });
