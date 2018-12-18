ca28a0d3a938b5a77f510f3595cd746f3d61701d
Rustam_Samigullin <rstsmg1@gmail.com>
2018-12-18 15:10:21 -0500

    123123123

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const cors = require('cors');
 require('./config/passport');
 
 mongoose
-  .connect('mongodb://localhost/app', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
