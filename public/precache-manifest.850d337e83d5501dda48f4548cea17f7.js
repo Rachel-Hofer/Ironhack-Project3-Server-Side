c6969dd0e878d35b27a1e30109aa8091b4d8f1b0
Rustam_Samigullin <rstsmg1@gmail.com>
2018-12-19 18:08:42 -0500

    1

---
+++ public/precache-manifest.850d337e83d5501dda48f4548cea17f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e161652784f75da090b",
+    "url": "/static/css/main.c95e518f.chunk.css"
+  },
+  {
+    "revision": "3e161652784f75da090b",
+    "url": "/static/js/main.3e161652.chunk.js"
+  },
+  {
+    "revision": "b788233f39e4df0a1fb0",
+    "url": "/static/js/1.b788233f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9e65ae4baffdb40e853e6a9da2c4f3fc",
+    "url": "/index.html"
+  }
+];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0deb4e9a025454bb80d7d56a97cac1d1.js"
+  "/precache-manifest.850d337e83d5501dda48f4548cea17f7.js"
 );
 
 workbox.clientsClaim();
